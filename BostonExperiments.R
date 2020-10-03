d31e27446a4f35f0e0611170c8433ec21eccce1c
Främling Kary <Kary.Framling@cs.umu.se>
2020-10-03 10:56:57 +0300

    Fixed handling of caret models for regression tasks in CIU. Cleaned up Boston Housing experiments file and added LIME+CIU there.

--- BostonExperiments.R
+++ BostonExperiments.R
@@ -29,94 +29,6 @@ require(caret) # Used here for data preparation functionalities.
 
 source("RBF.R")
 
-# Prepare data
-# caret preprocessing: 
-# https://www.machinelearningplus.com/machine-learning/caret-package/#3datapreparationandpreprocessing
-n.in <- ncol(Boston) - 1
-n.out <- 1
-preProcess_range_model <- preProcess(Boston, method='range') # Everything into [0,1]
-t.in <- as.matrix(predict(preProcess_range_model, newdata = Boston)[,1:n.in]) # Normalised inputs
-targets <- as.matrix(Boston$medv) # Original target values
-trainRowNumbers <- createDataPartition(targets, p=0.8, list=FALSE)
-train.in <- t.in[trainRowNumbers,]
-test.in <- t.in[-trainRowNumbers,]
-train.targets <- targets[trainRowNumbers,]
-test.targets <- targets[-trainRowNumbers,]
-
-# Create net and train
-rbf <- rbf.new(n.in, n.out, 0, normalize=TRUE, spread=0.1)
-n.hidden <- train.inka(rbf, train.in, train.targets, c=0.01, max.iter=nrow(train.in), 
-                       inv.whole.set.at.end=F, rmse.limit=2.5)
-print(paste("Hidden: ",n.hidden))
-test.out <- rbf$eval(test.in)
-#RMSE(test.targets,test.out) # How well did we do in practice?
-root.mean.squared.error(test.out, test.targets)
-
-# Same with Gradient Boosting
-require(gbm)
-Boston.boost=gbm(medv ~ . ,data = Boston[trainRowNumbers,],distribution = "gaussian",n.trees = 10000,
-                 shrinkage = 0.01, interaction.depth = 4)
-n.trees = seq(from=100 ,to=10000, by=100) # Don't know what this does in reality?
-predmatrix<-predict(Boston.boost,Boston[-trainRowNumbers,],n.trees=n.trees)
-#RMSE(as.matrix(Boston$medv)[-trainRowNumbers,],predmatrix)
-root.mean.squared.error(predmatrix,as.matrix(Boston$medv)[-trainRowNumbers,])
-
-# Caret version of gradient boosting
-kfoldcv <- trainControl(method="cv", number=10)
-caret.gbm <- train(medv ~ ., Boston[trainRowNumbers,], method="gbm", trControl=kfoldcv)
-test.out <- predict(caret.gbm, newdata=Boston[-trainRowNumbers,])
-print(paste("Caret GBM RMSE:", root.mean.squared.error(test.out, test.targets)))
-
-# Try out a set of RBFs and take best one. Takes a little longer...
-n.rbfs.to.test <- 10
-rbf <- find.best.inka (n=n.rbfs.to.test, train.inputs=train.in, train.outputs=train.targets, max.iter=200, 
-                       inv.whole.set.at.end=F, classification.error.limit=NULL, 
-                       rmse.limit=2.5, activation.function=squared.distance.activation, 
-                       output.function=imqe.output.function, nrbf=T, use.bias=F, 
-                       spread=0.1, c=0.01)
-test.out <- rbf$eval(test.in)
-root.mean.squared.error(test.out, test.targets)
-print(paste("Hidden: ",nrow(rbf$get.hidden()$get.weights())))
-
-# CIU
-source("ContextualImportanceUtility.R")
-in.mins <- apply(t.in, 2, min)
-in.maxs <- apply(t.in, 2, max)
-c.minmax <- cbind(in.mins, in.maxs)
-out.range <- matrix(c(min(targets), max(targets)), ncol=2)
-instance.values <- t.in[1,1:n.in] # Not very expensive
-# instance.values <- t.in[370,1:n.in] # Very expensive
-ciu.inka <- ciu.new(rbf, in.min.max.limits=c.minmax, abs.min.max=out.range, input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1])
-CI.CU <- ciu.inka$explain(instance.values, ind.inputs.to.explain=c(1), montecarlo.samples = 100)
-ciu.inka$barplot.CI.CU(instance.values)
-#ciu$barplot.CI.CU(instance.values, neutral.CU=-0.001)
-
-# CIU with Gradient Boosting
-in.mins <- apply(Boston[,1:n.in], 2, min)
-in.maxs <- apply(Boston[,1:n.in], 2, max)
-c.minmax <- cbind(in.mins, in.maxs)
-predict.function <- function(model, inputs) { predict(model,inputs,n.trees=10000) }
-ciu <- ciu.new(Boston.boost, in.min.max.limits=c.minmax, abs.min.max=out.range, 
-               input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1], 
-               predict.function=predict.function)
-inst.ind <- 1
-CI.CU <- ciu$explain(Boston[inst.ind,], ind.inputs.to.explain=c(1), montecarlo.samples = 100)
-ciu$barplot.CI.CU(Boston[inst.ind,1:n.in])
-
-# CIU with Gradient Boosting, FunctionApproximator wrapper
-gbm.fa.new <- function(gbm, n.trees=1) {
-  o.gbm <- gbm
-  o.n.trees <- n.trees
-  pub <- list(eval = function(inputs) { predict(o.gbm,inputs,n.trees=o.n.trees) })
-  class(pub) <- c("gbm.fa",class(function.approximator.new()),class(pub))
-  return(pub)
-}
-gbm.fa <- gbm.fa.new(Boston.boost, 10000)
-ciu.gbm.fa <- ciu.new(gbm.fa, in.min.max.limits=c.minmax, abs.min.max=out.range, 
-                      input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1])
-inst.ind <- 1
-CI.CU <- ciu.gbm.fa$explain(Boston[inst.ind,], ind.inputs.to.explain=c(1), montecarlo.samples = 100)
-ciu.gbm.fa$barplot.CI.CU(Boston[inst.ind,1:n.in])
 
 # Instance 406 has medv=5
 # Instance 370 has medv=50
@@ -125,19 +37,6 @@ ciu.gbm.fa$barplot.CI.CU(Boston[inst.ind,1:n.in])
 # Boston$medv[362] = 19.9
 # Boston$medv[199] = 34.6
 
-
-
-# LIME: can't get to work yet...
-# require(lime)
-# predict_model.gbm <- function(x, newdata, type, ...) {
-#   predict(x,newdata,n.trees=10000)
-# }
-# model_type.gbm <- function(x, ...) "regression"
-# explainer <- lime(Boston[,1:13], Boston.boost)
-# m<-Boston[c(406,6,370), 1:13]
-# explanation <- explain(m, explainer, n_features = 13)
-# print(explanation)
-# plot_features(explanation)
 
 # INKA 
 # spread=0.1, c=0.5 gives quite similar results as GBM but more hidden neurons. 
@@ -182,6 +81,24 @@ boston.lm.run <- function(train, test) {
                     row.names=c("lm")))
 }
 
+
+# Random Forest, caret 
+boston.rf.caret.run <- function(train, test) {
+  kfoldcv <- trainControl(method="cv", number=10)
+  exec.time <- system.time(
+    boston.rf.caret <<- train(medv ~ ., train, method="rf", trControl=kfoldcv))
+  # Training set performance
+  zvals <- predict(boston.rf.caret, newdata=train)
+  train.err <- RMSE(train$medv,zvals)
+  # Test set performance
+  zvals <- predict(boston.rf.caret, newdata=test)
+  test.err <- RMSE(test$medv,zvals)
+  return(data.frame("Train Set RMSE"=train.err, "Test Set RMSE"=test.err, "Execution time"=exec.time[3], 
+                    "Size"=boston.rf.caret$finalModel$ntree,
+                    row.names=c("rf")))
+}
+
+
 # GBM, caret 
 boston.gbm.caret.run <- function(train, test) {
   kfoldcv <- trainControl(method="cv", number=10)
@@ -219,8 +136,8 @@ boston.nnet.caret.run <- function(train, test) {
 boston.run.all <- function() {
   
   # Create training/test sets. 
-  preProcess_range_model <- preProcess(Boston[,-14], method='range') # Everything into [0,1]
-  Boston[,-14] <- predict(preProcess_range_model, newdata = Boston[,-14]) # Normalised inputs
+#  preProcess_range_model <- preProcess(Boston[,-14], method='range') # Everything into [0,1]
+#  Boston[,-14] <- predict(preProcess_range_model, newdata = Boston[,-14]) # Normalised inputs
   trainRowNumbers <- createDataPartition(Boston$medv, p=0.8, list=FALSE)
   training <- Boston[trainRowNumbers,]
   test <- Boston[-trainRowNumbers,]
@@ -228,6 +145,7 @@ boston.run.all <- function() {
   # Run all models and get performance
   boston.perf <- boston.inka.run(training, test)
   boston.perf <- rbind(boston.perf, boston.lm.run(training, test))
+  boston.perf <- rbind(boston.perf, boston.rf.caret.run(training, test))
   boston.perf <- rbind(boston.perf, boston.gbm.caret.run(training, test))
   boston.perf <- rbind(boston.perf, boston.nnet.caret.run(training, test))
   # boston.perf <- rbind(boston.perf, boston.nnet.fixed.run(training, test))
@@ -235,3 +153,171 @@ boston.run.all <- function() {
   
   return(boston.perf)
 }
+
+# Lime
+Boston.lime <- function(model, inst.ind=1) {
+  require(lime)
+  n.in <- ncol(Boston)-1
+  instance <- Boston[inst.ind,1:n.in]
+  explainer <- lime(Boston, model)
+  explanation <- explain(instance, explainer, n_features = n.in)
+  plot_features(explanation)
+}
+
+# CIU
+Boston.CIU <- function(model, inst.ind=1) {
+  source("ContextualImportanceUtility.R")
+  n.in <- ncol(Boston)-1
+  in.mins <- apply(Boston[,1:n.in], 2, min)
+  in.maxs <- apply(Boston[,1:n.in], 2, max)
+  c.minmax <- cbind(in.mins, in.maxs)
+  out.range <- matrix(c(min(Boston$medv), max(Boston$medv)), ncol=2)
+  
+  # We don't care about train/test set in this case because it's not about evaluating training performance.
+  ciu <<- ciu.new(model, in.min.max.limits=c.minmax, abs.min.max=out.range, 
+                     input.names=names(Boston)[1:n.in], output.names=paste("Instance #", inst.ind)) 
+  def.par <- par(no.readonly = TRUE) # save default, for resetting...
+  par(mfrow=c(1,1))
+  ciu$barplot.CI.CU(Boston[inst.ind,1:n.in], ind.output=1, sort="CI")
+  ciu$pie.CI.CU(Boston[inst.ind,1:n.in], ind.output=1, sort="CI")
+  par(mfrow=c(1,1))
+  for ( i in 1:n.in ) {
+    ciu$plot.CI.CU(Boston[inst.ind,1:n.in], ind.input=i, ind.output=1)
+  }
+  par(mfrow=c(1,1))
+  par(def.par)
+}
+
+boston.run.all()
+
+# Get the "standard" global importance estimate for all inputs
+varImp(boston.nnet.caret,scale = FALSE)
+
+# Also do a feature significance test using Recursive Feature Elimination
+control <- rfeControl(functions=rfFuncs, method="cv", number=10) # define the control using a random forest selection function
+results <- rfe(Boston[,1:13], Boston[,14], sizes=c(1:13), rfeControl=control) # run the RFE algorithm
+print(results) # summarize the results
+predictors(results) # list the chosen features
+plot(results, type=c("g", "o")) # plot the results
+
+# LIME and CIU plots. Especially "crim" is contradictory (and maybe others too)
+# It becomes even much more evident when using boston.nnet.caret as model. Seems 
+# like especially "chas" becomes completely crazy (sometimes or all of the time?)
+Boston.lime(boston.rf.caret,370)
+Boston.CIU(boston.rf.caret,370)
+Boston.lime(boston.nnet.caret,370)
+Boston.CIU(boston.nnet.caret,370)
+
+#===============================================================================
+# Old stuff here, kept just-in-case until sure that it can be cleaned away. 
+#===============================================================================
+
+# # Prepare data
+# # caret preprocessing: 
+# # https://www.machinelearningplus.com/machine-learning/caret-package/#3datapreparationandpreprocessing
+# n.in <- ncol(Boston) - 1
+# n.out <- 1
+# trainRowNumbers <- createDataPartition(targets, p=0.8, list=FALSE)
+# train.in <- Boston[trainRowNumbers,1:n.in]
+# test.in <- Boston[-trainRowNumbers,1:n.in]
+# train.targets <- Boston[trainRowNumbers,ncol(Boston)]
+# test.targets <- Boston[-trainRowNumbers,ncol(Boston)]
+
+# n.in <- ncol(Boston) - 1
+# n.out <- 1
+# preProcess_range_model <- preProcess(Boston, method='range') # Everything into [0,1]
+# t.in <- as.matrix(predict(preProcess_range_model, newdata = Boston)[,1:n.in]) # Normalised inputs
+# targets <- as.matrix(Boston$medv) # Original target values
+# trainRowNumbers <- createDataPartition(targets, p=0.8, list=FALSE)
+# train.in <- t.in[trainRowNumbers,]
+# test.in <- t.in[-trainRowNumbers,]
+# train.targets <- targets[trainRowNumbers,]
+# test.targets <- targets[-trainRowNumbers,]
+# 
+# # Create net and train
+# rbf <- rbf.new(n.in, n.out, 0, normalize=TRUE, spread=0.1)
+# n.hidden <- train.inka(rbf, train.in, train.targets, c=0.01, max.iter=nrow(train.in), 
+#                        inv.whole.set.at.end=F, rmse.limit=2.5)
+# print(paste("Hidden: ",n.hidden))
+# test.out <- rbf$eval(test.in)
+# #RMSE(test.targets,test.out) # How well did we do in practice?
+# root.mean.squared.error(test.out, test.targets)
+# 
+# # Same with Gradient Boosting
+# require(gbm)
+# Boston.boost=gbm(medv ~ . ,data = Boston[trainRowNumbers,],distribution = "gaussian",n.trees = 10000,
+#                  shrinkage = 0.01, interaction.depth = 4)
+# n.trees = seq(from=100 ,to=10000, by=100) # Don't know what this does in reality?
+# predmatrix<-predict(Boston.boost,Boston[-trainRowNumbers,],n.trees=n.trees)
+# #RMSE(as.matrix(Boston$medv)[-trainRowNumbers,],predmatrix)
+# root.mean.squared.error(predmatrix,as.matrix(Boston$medv)[-trainRowNumbers,])
+# 
+# # Caret version of gradient boosting
+# kfoldcv <- trainControl(method="cv", number=10)
+# caret.gbm <- train(medv ~ ., Boston[trainRowNumbers,], method="gbm", trControl=kfoldcv)
+# test.out <- predict(caret.gbm, newdata=Boston[-trainRowNumbers,])
+# print(paste("Caret GBM RMSE:", root.mean.squared.error(test.out, test.targets)))
+# 
+# # Try out a set of RBFs and take best one. Takes a little longer...
+# n.rbfs.to.test <- 10
+# rbf <- find.best.inka (n=n.rbfs.to.test, train.inputs=train.in, train.outputs=train.targets, max.iter=200, 
+#                        inv.whole.set.at.end=F, classification.error.limit=NULL, 
+#                        rmse.limit=2.5, activation.function=squared.distance.activation, 
+#                        output.function=imqe.output.function, nrbf=T, use.bias=F, 
+#                        spread=0.1, c=0.01)
+# test.out <- rbf$eval(test.in)
+# root.mean.squared.error(test.out, test.targets)
+# print(paste("Hidden: ",nrow(rbf$get.hidden()$get.weights())))
+# 
+# # CIU
+# source("ContextualImportanceUtility.R")
+# in.mins <- apply(t.in, 2, min)
+# in.maxs <- apply(t.in, 2, max)
+# c.minmax <- cbind(in.mins, in.maxs)
+# out.range <- matrix(c(min(targets), max(targets)), ncol=2)
+# instance.values <- t.in[1,1:n.in] # Not very expensive
+# # instance.values <- t.in[370,1:n.in] # Very expensive
+# ciu.inka <- ciu.new(rbf, in.min.max.limits=c.minmax, abs.min.max=out.range, input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1])
+# CI.CU <- ciu.inka$explain(instance.values, ind.inputs.to.explain=c(1), montecarlo.samples = 100)
+# ciu.inka$barplot.CI.CU(instance.values)
+# #ciu$barplot.CI.CU(instance.values, neutral.CU=-0.001)
+# 
+# # CIU with Gradient Boosting
+# in.mins <- apply(Boston[,1:n.in], 2, min)
+# in.maxs <- apply(Boston[,1:n.in], 2, max)
+# c.minmax <- cbind(in.mins, in.maxs)
+# predict.function <- function(model, inputs) { predict(model,inputs,n.trees=10000) }
+# ciu <- ciu.new(Boston.boost, in.min.max.limits=c.minmax, abs.min.max=out.range, 
+#                input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1], 
+#                predict.function=predict.function)
+# inst.ind <- 1
+# CI.CU <- ciu$explain(Boston[inst.ind,], ind.inputs.to.explain=c(1), montecarlo.samples = 100)
+# ciu$barplot.CI.CU(Boston[inst.ind,1:n.in])
+# 
+# # CIU with Gradient Boosting, FunctionApproximator wrapper
+# gbm.fa.new <- function(gbm, n.trees=1) {
+#   o.gbm <- gbm
+#   o.n.trees <- n.trees
+#   pub <- list(eval = function(inputs) { predict(o.gbm,inputs,n.trees=o.n.trees) })
+#   class(pub) <- c("gbm.fa",class(function.approximator.new()),class(pub))
+#   return(pub)
+# }
+# gbm.fa <- gbm.fa.new(Boston.boost, 10000)
+# ciu.gbm.fa <- ciu.new(gbm.fa, in.min.max.limits=c.minmax, abs.min.max=out.range, 
+#                       input.names=names(Boston)[1:n.in], output.names=names(Boston)[n.in+1])
+# inst.ind <- 1
+# CI.CU <- ciu.gbm.fa$explain(Boston[inst.ind,], ind.inputs.to.explain=c(1), montecarlo.samples = 100)
+# ciu.gbm.fa$barplot.CI.CU(Boston[inst.ind,1:n.in])
+
+# LIME: can't get to work yet...
+# require(lime)
+# predict_model.gbm <- function(x, newdata, type, ...) {
+#   predict(x,newdata,n.trees=10000)
+# }
+# model_type.gbm <- function(x, ...) "regression"
+# explainer <- lime(Boston[,1:13], Boston.boost)
+# m<-Boston[c(406,6,370), 1:13]
+# explanation <- explain(m, explainer, n_features = 13)
+# print(explanation)
+# plot_features(explanation)
+
